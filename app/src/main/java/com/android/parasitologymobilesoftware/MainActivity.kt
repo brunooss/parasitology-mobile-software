713081924c9dc57b88d387eaeec386187e1fc0d1
brunooss <bruno.oss@outlook.com>
2019-12-11 18:54:58 -0300

    Adição do Firebase-Analytics e FirebaseDatabase e migração para AndroidX

--- app/src/main/java/com/android/parasitologymobilesoftware/MainActivity.kt
+++ app/src/main/java/com/android/parasitologymobilesoftware/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.android.parasitologymobilesoftware
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val database: FirebaseDatabase = FirebaseDatabase.getInstance()
+        val reference = database.getReference("teste")
+
+        reference.setValue("teste do firebase no aplicativo :D")
     }
 }
