367198b49451fe3322c68ebb9337d8a66d4a2d48
brunooss <bruno.oss@outlook.com>
2020-01-13 18:32:32 -0300

    adição do arquivo SplashScreenActivity.java como Splash Screen

---
+++ app/src/main/java/com/android/parasitologymobilesoftware/SplashScreenActivity.java
@@ -0,0 +1,12 @@
+package com.android.parasitologymobilesoftware;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
