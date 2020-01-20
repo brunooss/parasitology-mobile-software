6339845de7a2d6f69e86369d6f23aab4d098f074
Lauro Cesar <lauroandradearaujo@gmail.com>
2020-01-19 21:31:19 -0300

    estilização: tela esqueci a senha, adição do cursor na cor branca e cor do texto dos EditText branca também

--- app/src/main/java/com/android/parasitologymobilesoftware/RecoverPasswordActivity.java
+++ app/src/main/java/com/android/parasitologymobilesoftware/RecoverPasswordActivity.java
@@ -1,10 +1,25 @@
 package com.android.parasitologymobilesoftware;
 
 import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.transition.Slide;
 
+import androidx.annotation.Nullable;
 
 
 public class RecoverPasswordActivity extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Slide moveScreen = new Slide();
+            moveScreen.setDuration(500);
 
+            getWindow().setEnterTransition(moveScreen);
+        }
 
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_recoverpassword);
+    }
 }
