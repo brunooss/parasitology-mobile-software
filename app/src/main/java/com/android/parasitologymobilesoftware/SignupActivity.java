1f0ad1e8be4aea238587085b6a1d94a4705e216d
brunooss <bruno.oss@outlook.com>
2020-01-20 10:30:46 -0300

    refatoração: melhorias na validação do nome e email no cadastro

--- app/src/main/java/com/android/parasitologymobilesoftware/SignupActivity.java
+++ app/src/main/java/com/android/parasitologymobilesoftware/SignupActivity.java
@@ -1,13 +1,7 @@
 package com.android.parasitologymobilesoftware;
 
-import android.content.Intent;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -24,14 +18,10 @@ import java.util.regex.Pattern;
 public class SignupActivity extends AppCompatActivity {
 
     /* Variables */
-            private FirebaseAuth firebaseAuth;
+    private FirebaseAuth firebaseAuth;
 
-            EditText textName;
-            EditText textEmail;
-            EditText textPassword;
-
-            boolean nameValidation = false;  String firstName;
-            boolean emailValidation = false;
+    EditText editTextName, editTextEmail, ediTextPassword;
+    TextView textViewNameError, textViewEmailError, textViewPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -40,54 +30,40 @@ public class SignupActivity extends AppCompatActivity {
 
         firebaseAuth = FirebaseAuth.getInstance();
 
-        textName = findViewById(R.id.editTextSignUpName);
-        textEmail = findViewById(R.id.editTextSignUpEmail);
-        textPassword = findViewById(R.id.editTextSignUpPassword);
+        editTextName = findViewById(R.id.editTextSignUpName);
 
-        textName.addTextChangedListener(new TextWatcher() {
+        editTextEmail = findViewById(R.id.editTextSignUpEmail);
+
+        ediTextPassword = findViewById(R.id.editTextSignUpPassword);
+
+        editTextName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(isNameValid(charSequence.toString())) {
-                    Toast.makeText(getBaseContext(), "Valid Name!!!", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getBaseContext(), "Invalid Name...", Toast.LENGTH_LONG).show();
+            public void onFocusChange(View view, boolean b) {
+                if(!b) { // If is not focused
+                    if(isNameValid(editTextName.getText().toString()))
+                        Toast.makeText(getBaseContext(), "Valid Name!!!", Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(getBaseContext(), "Invalid Name...", Toast.LENGTH_LONG).show();
                 }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
             }
         });
-        textEmail.addTextChangedListener(new TextWatcher() {
+        editTextEmail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                if(isEmailValid(charSequence.toString())) {
-                    Toast.makeText(getBaseContext(), "Valid Email!!!", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getBaseContext(), "Invalid Email...", Toast.LENGTH_LONG).show();
+            public void onFocusChange(View view, boolean b) {
+                if(!b) { // If is not focused
+                    if(isEmailValid(editTextName.getText().toString()))
+                        Toast.makeText(getBaseContext(), "Valid Email!!!", Toast.LENGTH_LONG).show();
+                    else
+                        Toast.makeText(getBaseContext(), "Invalid Email...", Toast.LENGTH_LONG).show();
                 }
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
             }
         });
 
         Spinner spinner = findViewById(R.id.spinnerSignUpYear);
-
-        ArrayAdapter<CharSequence> stringArrayAdapter = ArrayAdapter.createFromResource(this, R.array.years_array, R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> stringArrayAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.years_array,
+                R.layout.simple_spinner_item);
         stringArrayAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(stringArrayAdapter);
     }
@@ -95,29 +71,12 @@ public class SignupActivity extends AppCompatActivity {
 
     /* Methods */
     public boolean isNameValid(String name) { // Validates the name input, returning true if it's valid or false if it's not.
-
-        String regexName = "^(?:[\\\\p{Lu}&&[\\\\p{IsLatin}]])(?:(?:')?(?:[\\\\p{Ll}&&[\\\\p{IsLatin}]]))+(?:\\\\-(?:[\\\\p{Lu}&&[\\\\p{IsLatin}]])(?:(?:')?(?:[\\\\p{Ll}&&[\\\\p{IsLatin}]]))+)*(?: (?:(?:e|y|de(?:(?: la| las| lo| los))?|do|dos|da|das|del|van|von|bin|le) )?(?:(?:(?:d'|D'|O'|Mc|Mac|al\\\\-))?(?:[\\\\p{Lu}&&[\\\\p{IsLatin}]])(?:(?:')?(?:[\\\\p{Ll}&&[\\\\p{IsLatin}]]))+|(?:[\\\\p{Lu}&&[\\\\p{IsLatin}]])(?:(?:')?(?:[\\\\p{Ll}&&[\\\\p{IsLatin}]]))+(?:\\\\-(?:[\\\\p{Lu}&&[\\\\p{IsLatin}]])(?:(?:')?(?:[\\\\p{Ll}&&[\\\\p{IsLatin}]]))+)*))+(?: (?:Jr\\\\.|II|III|IV))?$"; //essa regex pega todos os caracteres até chegar no espaço
+        String regexName = "\\d+"; // Capture only numbers.
 
         Pattern pattern = Pattern.compile(regexName);
-        Matcher matcher = pattern.matcher(name);
+        Matcher m = pattern.matcher(name);
 
-        if (matcher.find()) {
-            nameValidation = true;
-        }
-        return nameValidation;
-    }
-    public String getFirstName(String name) { // If name is valid, returns the first name.
-        if (isNameValid(name)) {
-            String regexName = "\\S+";
-
-            Pattern pattern = Pattern.compile(regexName);
-            Matcher matcher = pattern.matcher(name);
-
-            //matcher.find();
-            firstName = matcher.group(0);
-            return matcher.group(0);
-        }
-        return null;
+        return !(name.startsWith(" ") || m.find()); // Returns true only if the name neither starts with a blank space nor has numbers.
     }
     public boolean isEmailValid(String email){ // Validates the email input, returning true if it's valid or false if it's not.
         String regexEmail = "^\\w*(\\.\\w*)?@\\w*\\.[a-z]+(\\.[a-z]+)?$";
@@ -125,17 +84,24 @@ public class SignupActivity extends AppCompatActivity {
         Pattern r = Pattern.compile(regexEmail);
         Matcher m = r.matcher(email);
 
-        if(m.find()){
-            emailValidation = true;
+        return m.find();
+    }
+    public String getFirstName(String name) { // If name is valid, returns the first name.
+        if (isNameValid(name)) {
+            String regexName = "\\S+"; //Capture only blank spaces.
+
+            Pattern pattern = Pattern.compile(regexName);
+            Matcher matcher = pattern.matcher(name);
+            if(matcher.find()) return matcher.group(0); else return name; // Returns the name before the blank space or the entire name.
         }
-        return emailValidation;
+        return null;
     }
 
     public void onButtonSignUpClick(View view) {
-        if(isEmailValid(textEmail.getText().toString()))
+        if(isEmailValid(editTextEmail.getText().toString()))
             Toast.makeText(this, "Email valid!", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Name valid! Name: " + getFirstName(textName.getText().toString()), Toast.LENGTH_LONG).show();
-        firebaseAuth.createUserWithEmailAndPassword(textEmail.getText().toString(), textPassword.getText().toString())
+        Toast.makeText(this, "Name valid! Name: " + getFirstName(editTextName.getText().toString()), Toast.LENGTH_LONG).show();
+        firebaseAuth.createUserWithEmailAndPassword(editTextEmail.getText().toString(), ediTextPassword.getText().toString())
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -143,7 +109,7 @@ public class SignupActivity extends AppCompatActivity {
 
                         UserProfileChangeRequest profileChangeRequest = new UserProfileChangeRequest
                                 .Builder()
-                                .setDisplayName(textName.getText().toString())
+                                .setDisplayName(editTextName.getText().toString())
                                 .build();
                         user.updateProfile(profileChangeRequest);
 
