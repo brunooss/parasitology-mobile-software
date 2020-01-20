a004c14edb498d957a3c59ea8b374f806b06c04f
Lauro Cesar <lauroandradearaujo@gmail.com>
2020-01-20 11:30:52 -0300

    funcionalidade: splash screen direciona usuário de acordo com a autenticação do Firebase

--- app/src/main/java/com/android/parasitologymobilesoftware/SplashScreenActivity.java
+++ app/src/main/java/com/android/parasitologymobilesoftware/SplashScreenActivity.java
@@ -5,23 +5,39 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class SplashScreenActivity extends AppCompatActivity {
 
     private Handler handler = new Handler();
 
+    /* Firebase instace variables */
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash_screen);
+
+        /* Initialize Firebase Auth */
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent =  new Intent(getBaseContext(), SigninActivity.class);
-                startActivity(intent);
+                Intent intentUnauthenticated =  new Intent(getBaseContext(), SigninActivity.class);
+                Intent intentAuthenticated = new Intent(getBaseContext(), MainActivity.class);
+                if(mFirebaseUser == null){                  // User is unauthenticated
+                    startActivity(intentUnauthenticated);  // Send him to SignIn Activity, so he can Authenticate
+                } else {                                        // User is authenticated
+                    startActivity(intentAuthenticated);         // Send him to Main Activity
+                }
                 finish();
             }
-        }, 4000);
+        }, 3000);
 
     }
 }
