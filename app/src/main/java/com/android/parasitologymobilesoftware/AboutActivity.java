dde13ff48d2db7134e7a4e1f14beab77216bb930
lauroandrade <lauroandradearaujo@gmail.com>
2020-05-03 18:40:51 -0300

    funcionalidade: adição de novas categorias clicáveis

--- app/src/main/java/com/android/parasitologymobilesoftware/AboutActivity.java
+++ app/src/main/java/com/android/parasitologymobilesoftware/AboutActivity.java
@@ -2,14 +2,19 @@ package com.android.parasitologymobilesoftware;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class AboutActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        String stringTest = "O &quot;Parasite Combat&quot; é um aplicativo cujo intuito é auxiliar os alunos a estudarem parasitologia de uma maneira mais rápida, eficaz e interativa!";
+
 
         final Toolbar toolbar = findViewById(R.id.toolbarAbout);
         toolbar.setTitle("SOBRE");
@@ -17,5 +22,7 @@ public class AboutActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+
     }
 }
--- app/src/main/java/com/android/parasitologymobilesoftware/CategoryFragment.java
+++ app/src/main/java/com/android/parasitologymobilesoftware/CategoryFragment.java
@@ -43,7 +43,7 @@ public class CategoryFragment extends Fragment {
 
             } else if (getArguments().getString("type").equals("text")) {
                 textViewTitle.setText(Html.fromHtml(this.getArguments().getString("title"), Html.FROM_HTML_MODE_COMPACT));
-                webViewText.loadData("<body><style>* {text-align: justify; }</style>" + this.getArguments().getString("text") + "</body>", "text/html", "UTF-8");
+                webViewText.loadData("<body><style> @font-face { font-family: Futura; src: url(\"file:///android_asset/font/futura_light_bt.ttf)\"} * {text-align: justify; font-family: Futura; }</style>" + this.getArguments().getString("text") + "</body>", "text/html", "UTF-8");
                 imageView.setVisibility(ImageView.INVISIBLE);
             }
         }
