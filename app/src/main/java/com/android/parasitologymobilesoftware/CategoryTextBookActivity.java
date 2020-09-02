1a8c9d1f0a085297e2308a30a4421202ed7cc41b
lauroandrade <lauroandradearaujo@gmail.com>
2020-09-02 00:19:03 -0300

    estilização: melhoria da UI da categoria e de vídeo e toolbar

--- app/src/main/java/com/android/parasitologymobilesoftware/CategoryTextBookActivity.java
+++ app/src/main/java/com/android/parasitologymobilesoftware/CategoryTextBookActivity.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.util.Log;
 import android.view.*;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -84,7 +83,7 @@ public class CategoryTextBookActivity extends AppCompatActivity {
 
 
         // Toolbar config
-        final Toolbar toolbar = findViewById(R.id.toolbarSubject);
+        final Toolbar toolbar = findViewById(R.id.toolbarSubjectCategoryMenu);
         toolbar.setTitle(category);
         toolbar.setTitleTextAppearance(this, R.style.FuturaMediumTextAppearance);
         setSupportActionBar(toolbar);
