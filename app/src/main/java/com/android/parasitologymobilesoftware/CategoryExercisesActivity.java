057f3c5461b0787fda599d0a8f41dc525f8ba8f4
lauroandrade <lauroandradearaujo@gmail.com>
2020-09-22 21:36:56 -0300

    alteração: adição da activity dos exercícios

---
+++ app/src/main/java/com/android/parasitologymobilesoftware/CategoryExercisesActivity.java
@@ -0,0 +1,19 @@
+package com.android.parasitologymobilesoftware;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CategoryExercisesActivity extends AppCompatActivity {
+
+    private String type, title, body, source, correctChoice, comment;
+    private String[] choices;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
