c5e0fdf0605431f320a3879efc5e82bbb83f62fb
lauroandrade <lauroandradearaujo@gmail.com>
2020-09-22 21:51:27 -0300

    alteração: recebendo nome e id da categoria

--- app/src/main/java/com/android/parasitologymobilesoftware/CategoryExercisesActivity.java
+++ app/src/main/java/com/android/parasitologymobilesoftware/CategoryExercisesActivity.java
@@ -1,19 +1,35 @@
 package com.android.parasitologymobilesoftware;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CategoryExercisesActivity extends AppCompatActivity {
 
+    // Our Activity TAG to LOG
+    private static final String TAG = "CategoryExercisesActivity";
+
+    // JSON Exercises attributes
     private String type, title, body, source, correctChoice, comment;
     private String[] choices;
+
+    // Parasitology Category
+    private String category;
+    private int categoryId;
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // This code snippet answers the following question: we want to show the exercises of which category?
+        Bundle extras = getIntent().getExtras();
+        category = extras.getString("category");
+        categoryId = extras.getInt("categoryId");
+        Log.i(TAG, "Category received: " +category);
+        Log.i(TAG, "Category Id received: " +categoryId);
 
     }
 }
